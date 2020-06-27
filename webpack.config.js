af95e483cd3f083b0d6b28e1a4d04c2408b82701
dade1987 <davidecavallini1987@gmail.com>
2020-06-27 11:29:25 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './client.js',
+  mode:'production',
   output: {path: path.resolve(__dirname, 'dist'), filename: 'bundle.js'},
   devServer: {
     historyApiFallback: true,
     noInfo: true,
     host: '0.0.0.0',
     disableHostCheck: true
+    
   }
 };
